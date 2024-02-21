f7670c8f626e3755a8a4bb9901ad221f5f677548
Naveed Shaik <shaiknaveed435@gmail.com>
2024-02-21 11:52:21 +0530

    added images

--- src/data.js
+++ src/data.js
@@ -2,12 +2,12 @@ const dummyData = [
     {
       title: "Lorem Ipsum 1",
       price: "19.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 2",
       price: "24.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 3",
@@ -17,27 +17,27 @@ const dummyData = [
     {
       title: "Lorem Ipsum 4",
       price: "29.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 5",
       price: "34.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 6",
       price: "39.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 7",
       price: "44.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 8",
       price: "49.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/1166473/pexels-photo-1166473.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 9",
@@ -47,37 +47,37 @@ const dummyData = [
     {
       title: "Lorem Ipsum 10",
       price: "59.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/906150/pexels-photo-906150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 11",
       price: "64.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/135940/pexels-photo-135940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 12",
       price: "69.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/1485548/pexels-photo-1485548.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 13",
       price: "74.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 14",
       price: "79.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/1181181/pexels-photo-1181181.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 15",
       price: "84.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/4482677/pexels-photo-4482677.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 16",
       price: "89.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/639086/pexels-photo-639086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 17",
@@ -87,17 +87,17 @@ const dummyData = [
     {
       title: "Lorem Ipsum 18",
       price: "99.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/53184/peacock-bird-plumage-color-53184.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 19",
       price: "104.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/133472/pexels-photo-133472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
       title: "Lorem Ipsum 20",
       price: "109.99",
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "https://images.pexels.com/photos/668465/pexels-photo-668465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
   ];
   
